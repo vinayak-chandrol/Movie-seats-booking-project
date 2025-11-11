4548be4935c55f037b868df694f5b397655d07cb
vinayak-chandrol <vinayakchandrol414@gmail.com>
2025-11-11 23:03:38 +0530

    Initial commit - setup React movie project

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { BrowserRouter as Routes,Route,Router } from 'react-router-dom'
+import Nav from './component/Nav'
+import { Link } from 'react-router-dom'
+function App() {
+  
+
+  return (
+    <>
+      <Nav />
+    </>
+  )
+}
+
+export default App
---
+++ src/component/Nav.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
+import { Link } from "react-router-dom";
+import logo from "../assets/cinemahublogo.jpg";
+import '../assets/Nav.css';
+const Nav = () => {
+  return (
+    <nav className="bg-black text-white px-10 py-4 flex items-center justify-between shadow-md">
+      {/* Left Section */}
+      <div className="flex items-center gap-8">
+        
+        <ul className="flex gap-6 text-lg">
+          <li></li>
+          <li>
+            <Link
+              to="/"
+              className="hover:text-yellow-400 transition duration-200"
+            >
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/movies"
+              className="hover:text-yellow-400 transition duration-200"
+            >
+              Movies
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/trending"
+              className="hover:text-yellow-400 transition duration-200"
+            >
+              Trending
+            </Link>
+          </li>
+        </ul>
+      </div>
+
+      {/* Right Section */}
+      <ul className="flex gap-6 text-lg">
+        <li>
+          <Link
+            to="/mylist"
+            className="hover:text-yellow-400 transition duration-200"
+          >
+            My List
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="/account"
+            className="hover:text-yellow-400 transition duration-200"
+          >
+            Account
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="/goldpass"
+            className="hover:text-yellow-400 transition duration-200 font-semibold"
+          >
+            GoldPass
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
