4c3ab96ea5236b0960bc965aa047674423e94874
vinayak-chandrol <vinayakchandrol414@gmail.com>
2025-11-19 11:38:48 +0530

    searchbar added

--- src/component/Nav.jsx
+++ src/component/Nav.jsx
@@ -1,70 +1,32 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import logo from "../assets/cinemahublogo.jpg";
-import Signup from "../Pages/Signup";
-import '../assets/Nav.css';
+import "../assets/Nav.css";
+
 const Nav = () => {
   return (
-    <nav className="bg-black text-white px-10 py-4 flex items-center justify-between shadow-md">
+    <nav className="nav">
+
       {/* Left Section */}
-      <div className="flex items-center gap-8">
-        
-        <ul className="flex gap-6 text-lg">
-          <li></li>
-          <li>
-            <Link
-              to="/"
-              className="hover:text-yellow-400 transition duration-200"
-            >
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="/movies"
-              className="hover:text-yellow-400 transition duration-200"
-            >
-              Movies
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="/trending"
-              className="hover:text-yellow-400 transition duration-200"
-            >
-              Trending
-            </Link>
-          </li>
+      <div className="left">
+        <ul className="links">
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/movies">Movies</Link></li>
+          <li><Link to="/trending">Trending</Link></li>
         </ul>
       </div>
 
+      {/* Search */}
+      <div className="search">
+        <input type="search" placeholder="Search movies..." />
+      </div>
+
       {/* Right Section */}
-      <ul className="flex gap-6 text-lg">
-        <li>
-          <Link
-            to="/mylist"
-            className="hover:text-yellow-400 transition duration-200"
-          >
-            My List
-          </Link>
-        </li>
-        <li>
-          <Link
-            to="/signup"
-            className="hover:text-yellow-400 transition duration-200"
-          >
-            Account
-          </Link>
-        </li>
-        <li>
-          <Link
-            to="/goldpass"
-            className="hover:text-yellow-400 transition duration-200 font-semibold"
-          >
-            GoldPass
-          </Link>
-        </li>
+      <ul className="links-right">
+        <li><Link to="/mylist">My List</Link></li>
+        <li><Link to="/signup">Account</Link></li>
+        <li><Link to="/goldpass">GoldPass</Link></li>
       </ul>
+
     </nav>
   );
 };
