72affee570436502475a26d5dc29f52a730c6fda
vinayak-chandrol <vinayakchandrol414@gmail.com>
2025-11-26 12:04:52 +0530

    done

--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -3,8 +3,10 @@ import "../assets/Signup.css";
 
 const Signup = () => {
   return (
-    <div className="signup-container">
-      <div className="left">
+     <div className="signup-wrapper">
+      <div className="left-section">
+       <div className="signup-container">
+     
       <h2 className="signup-title">Create Account</h2>
 
       <form className="signup-form">
@@ -25,9 +27,18 @@ const Signup = () => {
 
         <button type="submit">Sign Up</button>
       </form>
+     
+    </div>
+      </div>
+
+      <div className="right-section">
+     
+       <img src="https://i.pinimg.com/1200x/3e/ff/fc/3efffc4db594b1e93e9e51872b6cd22a.jpg" alt="movie banner" />
       </div>
     </div>
   );
 };
 
 export default Signup;
+
+    
